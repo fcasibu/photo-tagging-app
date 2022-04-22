a8750d77d7dfa9b139093377a2eaec8b8e737aae
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-22 21:07:30 +0800

    Add loading component
    Not really reusable

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../styles/Loading.module.css";
 
 const Loading = (Component) => {
-  return function checkLoad({ isLoading, imageURL }) {
-    if (!isLoading) {
-      return <Component imageURL={imageURL} />;
+  return function checkLoad({ isLoading, image }) {
+    if (!isLoading && image) {
+      return <Component url={image.url} name={image.name} />;
     }
 
     return <h1 className={styles.loading}>Loading...</h1>;
