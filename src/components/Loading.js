0a26259dbb1a43177c25cce89f562a00890a2df0
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-23 20:08:58 +0800

    Add context and backup name for direct url navigation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../styles/Loading.module.css";
+import CharacterContext from "../characters/CharacterContext";
+import { firestore } from "../firebase";
 
 const Loading = (Component) => {
-  return function checkLoad({ isLoading, image }) {
-    if (!isLoading && image) {
-      return <Component url={image.url} name={image.name} />;
-    }
+  return function checkLoad() {
+    const { images, selectedMap } = useContext(CharacterContext);
+    const [characters, setCharacters] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const backup = window.location.href.split("/").pop();
 
+    useEffect(() => {
+      const fetchData = async () => {
+        setIsLoading(true);
+        const res = firestore.collection(selectedMap || backup);
+        const data = await res.get();
+
+        data.docs.forEach((item) => {
+          setCharacters(item.data());
+        });
+        setIsLoading(false);
+      };
+
+      fetchData();
+    }, []);
+
+    if (!isLoading && images[selectedMap || backup]) {
+      return (
+        <Component
+          url={images[selectedMap || backup].url}
+          name={images[selectedMap || backup].name}
+          data={characters}
+        />
+      );
+    }
     return <h1 className={styles.loading}>Loading...</h1>;
   };
 };
