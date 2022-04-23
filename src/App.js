881f8d8474d4c717faf7f3c94d1ca9bdadc38f01
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-23 20:09:59 +0800

    Add provider and removed unnecessary lines

--- src/App.js
+++ src/App.js
@@ -1,89 +1,31 @@
-import React, { useState, useEffect } from "react";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import firebase from "firebase/compat/app";
-import { firestore, storage } from "./firebase";
 import Loading from "./components/Loading";
 import Home from "./components/Home/Home";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
 import GameScreen from "./components/GameScreen/GameScreen";
 import MainContent from "./components/Home/Content";
 import Header from "./components/Header/Header";
+import CharacterProvider from "./characters/CharacterProvider";
 
 const LoadGameScreen = Loading(GameScreen);
 
-const sliceText = (str) => {
-  return str.slice(0, str.lastIndexOf("."));
-};
-
 const App = () => {
-  const [images, setImages] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-
-  const getImage = async () => {
-    setIsLoading(false);
-    const response = await storage.ref().child("images").listAll();
-    const imageLinks = response.items.map(async (imageRef) => {
-      const url = await imageRef.getDownloadURL();
-      const name = sliceText(imageRef.name);
-      return { url, name };
-    });
-    return Promise.all(imageLinks);
-  };
-
-  const storeImages = async () => {
-    const links = await getImage();
-    links.forEach((link) => {
-      setImages((state) => {
-        return {
-          ...state,
-          [link.name]: {
-            name: link.name,
-            url: link.url,
-          },
-        };
-      });
-    });
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    storeImages();
-  }, []);
-
   return (
-    <React.Fragment>
+    <CharacterProvider>
       <Router basename="/">
         <Header />
         <Routes>
           <Route path="/" element={<Home />}>
-            <Route index element={<MainContent images={images} />} />
-            <Route
-              path="convention"
-              element={
-                <LoadGameScreen
-                  image={images.Convention}
-                  isLoading={isLoading}
-                />
-              }
-            />
-            <Route
-              path="beach"
-              element={
-                <LoadGameScreen image={images.Beach} isLoading={isLoading} />
-              }
-            />
-            <Route
-              path="future"
-              element={
-                <LoadGameScreen image={images.Future} isLoading={isLoading} />
-              }
-            />
+            <Route index element={<MainContent />} />
+            <Route path="convention" element={<LoadGameScreen />} />
+            <Route path="beach" element={<LoadGameScreen />} />
+            <Route path="future" element={<LoadGameScreen />} />
           </Route>
           <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </Router>
-    </React.Fragment>
+    </CharacterProvider>
   );
 };
 
