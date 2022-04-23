b7b50d133ae6c8ffbbd6cd2a4ded351ea9535ff4
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-24 00:23:19 +0800

    Remove unnecessary lines

--- src/components/GameScreen/helpers/getPosition.js
+++ src/components/GameScreen/helpers/getPosition.js
@@ -1,9 +1,7 @@
 const getPosition = (event) => {
-  const { left, top, width, height } = event.target.getBoundingClientRect();
-  const scaleX = width / event.target.width;
-  const scaleY = height / event.target.height;
-  const posX = Math.floor((event.clientX - left) * scaleX);
-  const posY = Math.floor((event.clientY - top) * scaleY);
+  const { left, top } = event.target.getBoundingClientRect();
+  const posX = Math.floor(event.clientX - left);
+  const posY = Math.floor(event.clientY - top);
 
   return { left, top, posX, posY };
 };
