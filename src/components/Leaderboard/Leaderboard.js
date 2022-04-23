5bb21a188e6060258333e8537c663d38252bcb6e
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-23 10:12:22 +0800

    Add Leaderboard

---
+++ src/components/Leaderboard/Leaderboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/Leaderboard.module.css";
+
+const Leaderboard = () => {
+  return (
+    <div className={styles.leaderboard}>
+      <h1>Leaderboard</h1>
+    </div>
+  );
+};
+
+export default Leaderboard;
