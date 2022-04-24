e1c738ccd3d4ac6395522e41e9ed0e8ce9ea8a64
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-24 17:04:47 +0800

    Add functionalities of form modal

--- src/components/Form/Modal.js
+++ src/components/Form/Modal.js
@@ -1,35 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import CharacterContext from "../../characters/CharacterContext";
 import styles from "../../styles/Form.module.css";
+import { firestore } from "../../firebase";
+import formatTime from "../../helper/formatTime";
 
 const Backdrop = ({ closeModal }) => {
   return <div className={styles.backdrop} onClick={closeModal}></div>;
 };
 
-const Form = ({ closeModal, endTime, resetTime }) => {
-  const submitHandler = (e) => {
+const Form = ({ closeModal, endTime, resetTime, map }) => {
+  const [submitting, setSubmitting] = useState(false);
+  const [name, setName] = useState("");
+  const leaderboardRef = firestore.collection("leaderboard");
+
+  const submitHandler = async (e) => {
     e.preventDefault();
+    setSubmitting(true);
+
+    await leaderboardRef.add({
+      map: map.selectedMap || map.backup,
+      name,
+      time: endTime,
+    });
+
     closeModal();
     resetTime();
+    setName("");
+    setSubmitting(false);
   };
 
   return (
     <div className={styles.form}>
       <h2>Good job!</h2>
-      <h3>Your time: {endTime}</h3>
+      <h3>Your time: {formatTime(endTime)}</h3>
       <form onSubmit={submitHandler}>
         <label htmlFor="name">Enter your name: </label>
-        <input id="name" type="text" />
-        <button type="submit">Submit to Leaderboard</button>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          maxLength="20"
+          required
+        />
+        <button type="submit">
+          {submitting ? "Submitting..." : "Submit to Leaderboard"}
+        </button>
       </form>
     </div>
   );
 };
 
 const Modal = () => {
-  const { closeModal, isFinished, endTime, resetTime } =
+  const { closeModal, isFinished, endTime, resetTime, backup, selectedMap } =
     useContext(CharacterContext);
 
   return (
@@ -41,6 +66,7 @@ const Modal = () => {
               closeModal={closeModal}
               endTime={endTime}
               resetTime={resetTime}
+              map={{ backup, selectedMap }}
             />,
             document.getElementById("modal")
           )}
@@ -62,6 +88,7 @@ Backdrop.propTypes = {
 
 Form.propTypes = {
   closeModal: PropTypes.func,
-  endTime: PropTypes.string,
+  endTime: PropTypes.number,
   resetTime: PropTypes.func,
+  map: PropTypes.object,
 };
